22239583d7f3a1793576833ca1627ebf18416df0
Carson Plant <carsonplant22@gmail.com>
2019-08-30 14:53:09 -0600

    hooked up image

--- app/models/weather.js
+++ app/models/weather.js
@@ -11,4 +11,15 @@ export default class Weather {
     this.city = data.name
     this.kelvin = data.main.temp
   }
+
+  get Template() {
+    return `
+        <div class="card">
+					<div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+					</div>
+        </div>
+        `
+  }
 }
